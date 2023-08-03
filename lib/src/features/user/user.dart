7556ac5433e6b1fda0aa131cbdd834bf19748347
Kripa Jethwani <kripa@atliq.com>
2023-08-03 13:37:47 +0530

    Initial commit

--- lib/src/features/user/user.dart
+++
@@ -1,3 +0,0 @@
-export 'package:equatable_demo/src/features/user/model/user_model.dart';
-export 'package:equatable_demo/src/features/user/provider/user_provider.dart';
-export 'package:equatable_demo/src/features/user/repository/user_repository.dart';
