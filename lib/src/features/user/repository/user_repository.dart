f7edca9c42a1357556c11dcc4ae475ff3d8882d2
Kripa Jethwani <kripa@atliq.com>
2023-08-03 10:17:09 +0530

    Project structure change

--- lib/src/features/user/repository/user_repository.dart
+++ lib/src/features/user/repository/user_repository.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:equatable_demo/src/constants/endpoints.dart';
 import 'package:equatable_demo/src/utils/api_client.dart';
-import 'package:equatable_demo/src/features/user/controller/user_controller.dart';
 import 'package:equatable_demo/src/features/user/model/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
--- lib/src/features/user/user.dart
+++ lib/src/features/user/user.dart
@@ -1,4 +1,3 @@
-export 'package:equatable_demo/src/features/user/controller/user_controller.dart';
 export 'package:equatable_demo/src/features/user/model/user_model.dart';
 export 'package:equatable_demo/src/features/user/provider/user_provider.dart';
 export 'package:equatable_demo/src/features/user/repository/user_repository.dart';
